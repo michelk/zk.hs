355e3db149c31d9696248d17c5c38c4fe7a8beb9
Michel Kuhlmann <kum@tkconsult.ch>
2020-05-20 10:46:47 +0200

    link file to zk-dir

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Control.Monad
-import Data.List
 import System.Directory
 import System.Environment
 import System.Exit
@@ -9,36 +8,56 @@ import System.FilePath.Posix
 import System.Process
 
 _ZK_DIR_NAME :: FilePath
-_ZK_DIR_NAME = ".neuron"
+_ZK_DIR_NAME = ".zk"
 
 main :: IO ()
 main = do
   args <- getArgs
   when
-    (length args == 0 || (length args == 1 && head args `elem` ["-h", "--help"]))
+    (null args || (length args == 1 && head args `elem` ["-h", "--help"]))
     ( putStrLn
-        "Wrapper for 'neuron' which searches for a '.neuron' directory \
-        \ as zettelkasten in the descending directory tree"
+        ( "Wrapper for 'neuron' which searches for a" <> _ZK_DIR_NAME
+            <> "directory \
+               \ as zettelkasten in the descending directory tree"
+        )
         >> exitSuccess
     )
   pwd <- getCurrentDirectory
   let ds = splitDirectories pwd
   zkDir <- findZkDir _ZK_DIR_NAME ds
-  ext <-
+  _ <-
     case zkDir of
-      Nothing -> do
-        die ("Could not find directory " <> _ZK_DIR_NAME)
+      Nothing -> die ("Could not find directory " <> _ZK_DIR_NAME)
       Just d -> do
-        let prog = ["neuron", "-d", d]
-        let cmd = intercalate " " (prog ++ args)
-        system cmd
+        let prog = unwords ["neuron", "-d", d]
+        case head args of
+          "link" -> do
+            when (length args /= 2) (error "Please provide a file to link")
+            let linkFile = args !! 1
+            neuronFile <- readProcess prog ["new", "DUMMY"] []
+            createFileLink linkFile neuronFile
+            exitSuccess
+          "dir" -> do
+            isSymLink <- pathIsSymbolicLink d
+            d' <-
+              if isSymLink
+                then (dropFileName d </>) <$> getSymbolicLinkTarget d
+                else return d
+            putStrLn d'
+            exitSuccess
+          _ -> do
+            let cmd = unwords ([prog] ++ map quote args)
+            system cmd
   return ()
 
 findZkDir :: FilePath -> [FilePath] -> IO (Maybe FilePath)
 findZkDir _ [] = return Nothing
 findZkDir zkdir ds = do
-  let dAbs = joinPath ds
-  found <- doesDirectoryExist (dAbs </> zkdir)
+  let dAbs = joinPath (ds ++ [zkdir])
+  found <- doesDirectoryExist dAbs
   if found
     then return (Just dAbs)
     else findZkDir zkdir (init ds)
+
+quote :: String -> String
+quote x = mconcat ["'", x, "'"]
