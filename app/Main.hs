fd17032791503c3f798dcfa7b670ddeae64f6846
Michel Kuhlmann <kum@tkconsult.ch>
2020-05-21 08:52:22 +0200

    linking single files into zettelkasten

--- app/Main.hs
+++ app/Main.hs
@@ -26,43 +26,79 @@ main = do
         >> exitSuccess
     )
   pwd <- getCurrentDirectory
-  let ds = splitDirectories pwd
-  zkDir <- findZkDir _ZK_DIR_NAME ds
-  _ <-
-    case zkDir of
-      Nothing -> die ("Could not find directory " <> _ZK_DIR_NAME)
-      Just d -> do
-        let prog = unwords [_NEURON_EXE, "-d", d]
-        case head args of
-          "link" -> do
-            when (length args /= 2) (error "Please provide a file to link")
-            let linkFile = args !! 1
-            neuronFile <-
-              head . lines
-                <$> readProcess _NEURON_EXE ["-d", d, "new", "DUMMY"] []
-            createFileLink linkFile neuronFile
-            exitSuccess
-          "dir" -> do
-            isSymLink <- pathIsSymbolicLink d
-            d' <-
-              if isSymLink
-                then (dropFileName d </>) <$> getSymbolicLinkTarget d
-                else return d
-            putStrLn d'
-            exitSuccess
-          _ -> do
-            let cmd = unwords ([prog] ++ map quote args)
-            system cmd
+  zkdir <- getZkDir pwd
+  case head args of
+    "link" -> do
+      when (length args /= 2) (error "Please provide a file to link")
+      let linkFile = args !! 1
+      linkFileToZk linkFile
+      exitSuccess
+    _ -> do
+      let prog = unwords [_NEURON_EXE, "-d", zkdir]
+      let cmd = unwords ([prog] ++ map quote args)
+      system cmd
   return ()
-
-findZkDir :: FilePath -> [FilePath] -> IO (Maybe FilePath)
-findZkDir _ [] = return Nothing
-findZkDir zkdir ds = do
-  let dAbs = joinPath (ds ++ [zkdir])
-  found <- doesDirectoryExist dAbs
-  if found
-    then return (Just dAbs)
-    else findZkDir zkdir (init ds)
 
 quote :: String -> String
 quote x = mconcat ["'", x, "'"]
+
+linkFileToZk :: FilePath -> IO ()
+linkFileToZk f = do
+  pwd <- getCurrentDirectory
+  rootdir <- errorOnNonExistence <$> getZkRootDir pwd
+  zkreldir <- getZkRelDir rootdir
+  let pwdreldir = getRelPathRoot rootdir pwd
+  newzf <- neuronNewZettel (rootdir </> zkreldir)
+  removePathForcibly newzf
+  createFileLink
+    (relPathToDescTree zkreldir </> pwdreldir </> f)
+    newzf
+
+errorOnNonExistence :: Maybe FilePath -> FilePath
+errorOnNonExistence Nothing = error "zk-directory not found"
+errorOnNonExistence (Just d) = d
+
+getZkDir :: FilePath -> IO FilePath
+getZkDir d = do
+  zkrootdir <- getZkRootDir d
+  return ((errorOnNonExistence zkrootdir) </> _ZK_DIR_NAME)
+
+getZkRootDir' :: [FilePath] -> IO (Maybe FilePath)
+getZkRootDir' [] = return Nothing
+getZkRootDir' ds = do
+  let p = joinPath ds
+  let dAbs = p </> _ZK_DIR_NAME
+  found <- doesDirectoryExist dAbs
+  if found
+    then return (Just p)
+    else getZkRootDir' (init ds)
+
+getZkRootDir :: FilePath -> IO (Maybe FilePath)
+getZkRootDir f = getZkRootDir' (splitDirectories f)
+
+getZkRelDir :: FilePath -> IO FilePath
+getZkRelDir rootdir = do
+  let zkdirname = _ZK_DIR_NAME
+  let zkdir = rootdir </> zkdirname
+  isSymLinkZkDir <- pathIsSymbolicLink zkdir
+  if isSymLinkZkDir
+    then getSymbolicLinkTarget zkdir
+    else return zkdirname
+
+getRelPathRoot :: FilePath -> FilePath -> FilePath
+getRelPathRoot rootdir d = diffPath d rootdir
+
+diffPath :: FilePath -> FilePath -> FilePath
+diffPath x y = joinPath $ drop (length ys) xs
+  where
+    xs = splitDirectories x
+    ys = splitDirectories y
+
+neuronNewZettel :: FilePath -> IO FilePath
+neuronNewZettel d =
+  head . lines <$> readProcess _NEURON_EXE ["-d", d, "new", "DUMMY"] []
+
+relPathToDescTree :: FilePath -> FilePath
+relPathToDescTree f = joinPath . take n . repeat $ ".."
+  where
+    n = length . splitDirectories $ f
